b452060e7377cba8e21d7fcc2c669ac8fd43f061
munnaMia <mdmunnamia987@gmail.com>
2026-04-04 10:01:50 +0600

    learn curd with rest style

--- resources/views/ideas/show.blade.php
+++ resources/views/ideas/show.blade.php
@@ -4,5 +4,9 @@
         {{ $idea->Description }}
     </div>
 
+    <div class="mt-6 flex items-center justify-end gap-x-6">
+        <a href="/ideas/{{ $idea->id }}/edit"
+            class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Edit</a>
+    </div>
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,30 @@ Route::get('/ideas/{idea}', function (Idea $idea) {
     ]);
 });
 
+// edit
+Route::get('/ideas/{idea}/edit', function (Idea $idea) {
+    return view('ideas.edit', [
+        'idea' => $idea
+    ]);
+});
+
+// update
+Route::patch('/ideas/{idea}', function (Idea $idea) {
+    $idea->update([
+        'description' => request('description'),
+    ]);
+
+    return redirect('/ideas/' . $idea->id);
+});
+
+// destroy
+Route::delete('/ideas/{idea}', function (Idea $idea) {
+    $idea->delete();
+
+    return redirect('/ideas');
+});
+
+// store  
 Route::post('/ideas', function () {
     $idea = request('idea');
 
@@ -26,11 +50,5 @@ Route::post('/ideas', function () {
         'state' => 'pending'
     ]);
 
-    return redirect('/ideas');
-});
-
-
-Route::get('/delete_ideas', function () {
-    Idea::truncate();
     return redirect('/ideas');
 });
