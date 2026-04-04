0d53e55de703bce3e9349365ff05c6a1be1a8607
munnaMia <mdmunnamia987@gmail.com>
2026-04-04 10:44:16 +0600

    learn controllers

---
+++ resources/views/ideas/create.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+
+    <form method="POST" action="/ideas">
+        @csrf
+
+        <div class="col-span-full">
+            <label for="idea" class="block text-sm/6 text-white font-bold">new idea</label>
+            <div class="mt-2">
+                <textarea id="idea" name="idea" rows="3" placeholder="Write somthing"
+                    class="border w-full block  rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"></textarea>
+            </div>
+        </div>
+
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <button type="submit"
+                class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Save</button>
+        </div>
+    </form>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,12 @@
 <?php
 
-use App\Models\Idea;
+use App\Http\Controllers\IdeaContorller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/ideas', function () {
-    // $ideas = DB::table('ideas')->get();
-    $ideas = Idea::all();
-
-    return view('ideas/index', [
-        'idea' => $ideas
-    ]);
-});
-
-Route::get('/ideas/{idea}', function (Idea $idea) {
-    return view('ideas.show', [
-        'idea' => $idea
-    ]);
-});
-
-// edit
-Route::get('/ideas/{idea}/edit', function (Idea $idea) {
-    return view('ideas.edit', [
-        'idea' => $idea
-    ]);
-});
-
-// update
-Route::patch('/ideas/{idea}', function (Idea $idea) {
-    $idea->update([
-        'description' => request('description'),
-    ]);
-
-    return redirect('/ideas/' . $idea->id);
-});
-
-// destroy
-Route::delete('/ideas/{idea}', function (Idea $idea) {
-    $idea->delete();
-
-    return redirect('/ideas');
-});
-
-// store  
-Route::post('/ideas', function () {
-    $idea = request('idea');
-
-    Idea::create([
-        'description' => $idea,
-        'state' => 'pending'
-    ]);
-
-    return redirect('/ideas');
-});
+Route::get('/ideas', [IdeaContorller::class, 'index']);
+Route::get('/ideas/create', [IdeaContorller::class, 'show']);
+Route::post('/ideas', [IdeaContorller::class, 'store']);
+Route::get('/ideas/{idea}', [IdeaContorller::class, 'show']);
+Route::get('/ideas/{idea}/edit', [IdeaContorller::class, 'edit']);
+Route::patch('/ideas/{idea}', [IdeaContorller::class, 'update']);
+Route::delete('/ideas/{idea}', [IdeaContorller::class, 'destroy']);
